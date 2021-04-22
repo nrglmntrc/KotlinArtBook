5bca2b0371ce08b04b2f971be07681f08deefb4e
Nurgül Mantarcı <nurgulmantarci28@gmail.com>
2021-04-22 13:10:27 +0300

    fragment list and fragment add created

---
+++ app/src/main/java/com/nurgulmantarci/kotlinartbook/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.nurgulmantarci.kotlinartbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here.
+        val id = item.getItemId()
+
+        if (id == R.id.action_add) {
+
+            return true
+        }
+
+
+        return super.onOptionsItemSelected(item)
+
+    }
+}
