0fdffb787289d0851306ad3fb2b9186552e6fe41
Nurgül Mantarcı <nurgulmantarci28@gmail.com>
2021-04-22 13:23:32 +0300

    layout design done.

--- app/src/main/java/com/nurgulmantarci/kotlinartbook/MainActivity.kt
+++ app/src/main/java/com/nurgulmantarci/kotlinartbook/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.Menu
 import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
